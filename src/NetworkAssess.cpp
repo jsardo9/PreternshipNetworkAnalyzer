5b93fd7df4627eaf9cba308a510d7ce866a67763
jsardo9 <69776122+jsardo9@users.noreply.github.com>
2020-10-29 17:06:24 -0400

    Update NetworkAssess.cpp

--- src/NetworkAssess.cpp
+++ src/NetworkAssess.cpp
@@ -14,18 +14,40 @@
 
 
 int main() {
+//---------Key Data Structures and Variables------------
+//------------------------------------------------------
+list<int> networkLats; //Stores recorded network latencies in each node
+int tInterval; //Time interval between each recorded latency from "networkLats"
+
+
+
 //----Reading in NetworkLog.txt and storing it in DLL---
 //------------------------------------------------------
-  list<int> networkEvents;
+fstream networkLog;
+int nLats; //number of latencies in the network log
+networkLog.open("../include/NetworkLog.txt", ios::in)
 
-  fstream networkLog;
-  networkLog.open("../include/NetworkLog.txt", ios::in)
-  string netEvent;
-      while(getline(networkLog, netEvent)){  //read data from file object and put it into string.
-        // add to list
-      }
-  networkLog.close; //closing Network Log txt file
+// retreving first line from NetworkLog.txt which contains information regarding
+// total time contained in log (hrs) and the time interval between recordings (seconds)
+string hrs;
+getline(networkLog, hrs, '\t');
+string interval;
+getline(networkLog, interval);
+
+  
+tInterval = (int)interval;
+nLats = ((int)hours) * 3600)/ tInterval;
+
+// reading latencies into networkLats
+string currLatency;
+    for (int i = 0; i < nLats){  //read data from file object and put it into string.
+       getline(networkLog, currLatency)
+       networkLats.push_back((int)currLatency)
+    }
+
+networkLog.close; //closing Network Log txt file
+//------------------------------------------------------
+//------------------------------------------------------
 
   return 0;
 }
-
