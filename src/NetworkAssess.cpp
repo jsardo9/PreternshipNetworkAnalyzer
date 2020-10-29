cf5f4423341fce50da30bf2c869d96773af017a0
jsardo9 <69776122+jsardo9@users.noreply.github.com>
2020-10-29 16:19:33 -0400

    Update NetworkAssess.cpp

--- src/NetworkAssess.cpp
+++ src/NetworkAssess.cpp
@@ -1,1 +1,31 @@
-// Main function in here
+/*************************************
+ * File name: NetworkAssess.cpp
+ * Authors: Jacob Sardo and Chase Brown
+ * Emails: jsardo@nd.edu and cbrown54@nd.edu
+ *
+ * This file contains the main driver Function
+ * in which pattern recognition is applied to
+ * flagged high latency times in the Network Log
+ * ***********************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+
+
+int main() {
+//----Reading in NetworkLog.txt and storing it in DLL---
+//------------------------------------------------------
+  list<int> networkEvents;
+
+  fstream networkLog;
+  networkLog.open("../include/NetworkLog.txt", ios::in)
+  string netEvent;
+      while(getline(networkLog, netEvent)){  //read data from file object and put it into string.
+        // add to list
+      }
+  networkLog.close; //closing Network Log txt file
+
+  return 0;
+}
+
