2b4729bbe043d17e0ff8f6d10ce30b8acefa958d
Chase Brown <rappybrown@gmail.com>
2020-10-29 23:52:49 -0400

    params

--- DataGen/include/generator.h
+++ DataGen/include/generator.h
@@ -7,12 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <cstdlib>
+#include <fstream>
 
 #define COUT std::cout;
 #define ENDL std::endl;
+#define FST std::ofstream;
 
-int generate() {
 
+int generate(int time, int freq, int expected) {
+    srand(time(NULL));
+
+    int num = (time * 60 * 60) / freq;
+    int prev = 0;
+    FST data("data.txt");
+
+    data << num << "\t" << time << "\t" << freq << '\n';
+
+    for(int i = 0; i < num; i++) {
+        int lat = 0;
+
+        if(prev >= ((1 / (freq / 60)) * 30) & (rand() % 10) == 1 ) {
+            prev = 0;
+        }
+        else {
+            prev++;
+        }
+
+
+        data << lat << '\n';
+    }
+    data.close();
+    return numData;
 }
 
 
--- DataGen/src/generate.cpp
+++ DataGen/src/generate.cpp
@@ -6,14 +6,24 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    if(argc != 3) {
+    if(argc != 4) {
         COUT << "Incorrect arguements" << ENDL;
         COUT << "example: ./generate [time] [frequency]" << ENDL;
         COUT << "[time] = int value in hours ex. 24 or 48" << ENDL;
         COUT << "[frequency] = int value in seconds ex. 60 or 15" << ENDL;
+        COUT << "[expected] = int value in hours ex. 12 or 24 **[time] % [expected] == 0**" << ENDL
         COUT << "If we run './generate 12 60' this will generate 360 lines of data" << ENDL;
         COUT << "one line for each 60 seconds from 0 to 6 hours."
         return 0;
+    }
+    else {
+        int time = atoi(argv[1]);
+        int freq = atoi(argv[2]);
+        int expected = atoi(argv[3]);
+        int numData = 0;
+        numData = generate(time, freq, expected);
+
+        COUT << numData << " data entries were generated" << ENDL;
     }
 
     return 0;
