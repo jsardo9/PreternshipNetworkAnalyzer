7f189f1babff8fecefdf389edafa724b523ef286
chaseBrow <46569903+chaseBrow@users.noreply.github.com>
2020-10-30 11:30:48 -0400

    fixed makefile

--- DataGen/include/generator.h
+++ DataGen/include/generator.h
@@ -12,26 +12,54 @@
 
 #define COUT std::cout;
 #define ENDL std::endl;
-#define FST std::ofstream;
+#define CFST std::ofstream;
+#define FST std::fstream;
 
+int addOrSub() {
+    int random = rand() % 2; //0 = sub, 1 = add;
+    if(random) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
 
+int genLat(int lat) {
+    int change = rand() % 20;
+    int dir = addOrSub();
+    if(dir) {
+        lat += change; 
+    }
+    else {
+        lat -= change;
+    }
+    return lat;
+}
 
-int genSpike(fstream& data, int freq) {
-    int numAdded;
+int genSpike(FST& data, int freq) {
+    int numAdded = 0;
     int spikeTime = 60 * ((rand() % 5) + 3); //average time for a spike is 2 - 7 min (converted to seconds)
-
+    int lat = (rand() % 500) + 150; //generate random latency between 150 - 650
+    while(spikeTime >= freq) {
+        data << lat << "\n";
+        lat = genLat(lat);
+        spikeTime -= freq;
+        numAdded++;
+    }
     return numAdded;
 }
 
 int generate(int time, int freq, int expected) {
     srand(time(NULL));
-
+    //num is going to be the number of data points generated
     int num = (time * 60 * 60) / freq;
-    
 
-    
+    //prev is to make sure we do not generate spikes on top of one another... they need to have some space between them in order to 
+    //create the most accurate results.
     int prev = 0;
-    FST data("data.txt");
+    CFST data;
+    data.open("data.txt", ios::app); //opens the file in append mode
 
     data << num << "\t" << time << "\t" << freq << '\t' << expected << "\n";
     int lat = 0;
@@ -39,29 +67,18 @@ int generate(int time, int freq, int expected) {
         //this is checking to see if the previous was greater than 30min ago and has a random 1/10 chance of triggering a spike in latency.
         //OR if there is an expected spike.
         if((prev >= ((1 / (freq / 60)) * 30) && (rand() % 10) == 1 ) ||  time % expected == 0) {  
-            int add = genSpike();
+            int add = genSpike(data, freq);
             i += add - 1;
             prev = 0;
         }
         else {
-            int change = rand() % 20;
-            int addOrSub = rand() % 2; //0 = sub, 1 = add;
-            if(addOrSub) {
-                lat += change; 
-            }
-            else {
-                lat -= change;
-            }
+            lat = genLat(lat);
             data << lat << '\n';
             prev++;
         }
-
-
     }
     data.close();
-    return numData;
+    return num;
 }
-
-
 
 #endif
