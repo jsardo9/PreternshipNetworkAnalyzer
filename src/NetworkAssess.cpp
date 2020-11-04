9ae5d3a19f37e20a3220e8d801ff634ad2a0b8bc
jsardo9 <69776122+jsardo9@users.noreply.github.com>
2020-11-04 10:12:45 -0500

    Update NetworkAssess.cpp

--- src/NetworkAssess.cpp
+++ src/NetworkAssess.cpp
@@ -1,7 +1,7 @@
 /*************************************
  * File name: NetworkAssess.cpp
- * Authors: Jacob Sardo
- * Emails: jsardo@nd.edu
+ * Authors: Jacob Sardo and Chase Brown
+ * Emails: jsardo@nd.edu and cbrown54@nd.edu
  *
  * This file contains the main driver Function
  * in which pattern recognition is applied to
@@ -11,43 +11,63 @@
 #include <fstream>
 #include <string>
 #include <list>
+#include "../include/NetworkFunctions.h"
+#include "../include/Period.h"
 
 
 int main() {
+
 //---------Key Data Structures and Variables------------
 //------------------------------------------------------
-list<int> networkLats; //Stores recorded network latencies in each node
-int tInterval; //Time interval between each recorded latency from "networkLats"
+std::list<Period> networkPeriods; //Stores recorded network latencies in each node
 
 
+//----Reading in data.txt and storing flags in DLL---
+//-----Potentially put below code in a function-------
+std::fstream networkLog;
 
-//----Reading in NetworkLog.txt and storing it in DLL---
-//------------------------------------------------------
-fstream networkLog;
-int nLats; //number of latencies in the network log
-networkLog.open("../include/NetworkLog.txt", ios::in)
 
-// retreving first line from NetworkLog.txt which contains information regarding
-// total time contained in log (hrs) and the time interval between recordings (seconds)
-string hrs;
-getline(networkLog, hrs, '\t');
-string interval;
-getline(networkLog, interval);
+//!!!!!! Not sure how we get these values if they are not in first line of data.txt!!!!!!
+int tPeriod = 1; // length of individual periods (hours)
+int nLats = 20; //number of latencies in the network log
+int latsPerPeriod = 5; //number of latencies per period
 
-  
-tInterval = (int)interval;
-nLats = ((int)hours) * 3600)/ tInterval;
+networkLog.open("data.txt", std::ios::in);
 
 // reading latencies into networkLats
-string currLatency;
-    for (int i = 0; i < nLats){  //read data from file object and put it into string.
-       getline(networkLog, currLatency)
-       networkLats.push_back((int)currLatency)
+int currInPeriod = 0; // keeps track when we reach the end of a current period
+Period currPeriod;
+std::vector<Flag> currFlags; // list of flags within the current period
+bool withinFlag = false; //true while index is currently within a flag
+
+    for (int i = 0; i < nLats; ++i){
+      if (currInPeriod == 0) {
+         currPeriod = Period(tPeriod, currFlags); // Creating a new Period if we are at index 0 of a Period
+      }
+
+       //pulling current latency
+       std::string currLatency;
+       getline(networkLog, currLatency);
+       int currLat = std::stoi(currLatency);
+
+       if (currLat > 100) {
+         if (!withinFlag) { // this means its the start of a new flag
+           withinFlag = true;
+           currFLags = std::vector<Flag>();
+         }
+      currFlags.push_back((int)currLatency)
+     }
+     else if (withinFlag) {// end of a flag means we need to add flag to current period
+       withinFlag = false;
+
+     }
+
+       currInPeriod = (currInPeriod + 1) % latsPerPeriod; // increase the index of location in the period
     }
+//------------------------------------------------------
 
 networkLog.close; //closing Network Log txt file
-//------------------------------------------------------
-//------------------------------------------------------
+
 
   return 0;
 }
