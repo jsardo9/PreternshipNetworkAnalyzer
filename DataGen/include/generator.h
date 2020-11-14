52f6f3c2023929c0512857d652f50b5d47a791b5
Chase Brown <rappybrown@gmail.com>
2020-11-14 17:27:07 -0500

    dump

--- DataGen/include/generator.h
+++ DataGen/include/generator.h
@@ -1,8 +1,8 @@
-//Author: Chase Brown
+//Author: Chase Brown & Jacob Sardo
 //Email: cbrown54@nd.edu
 
-#ifndef GENERATE_H
-#define GENERATE_H
+#ifndef GENERATOR_H
+#define GENERATOR_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,9 +11,11 @@
 #include <iostream>
 #include <fstream>
 
-#define COUT std::cout;
-#define ENDL std::endl;
-// #define FST std::ofstream;
+#define COUT std::cout
+#define ENDL std::endl
+#define TM std::time
+#define APP std::ios_base::app
+#define FST std::ofstream
 
 int addOrSub() {
     int random = rand() % 2; //0 = sub, 1 = add;
@@ -40,9 +42,10 @@ int genLat(int lat) {
 int genSpike(int freq) {
     int numAdded = 0;
     int spikeTime = 60 * ((rand() % 5) + 3); //average time for a spike is 2 - 7 min (converted to seconds)
+    // COUT << "spike time: " << spikeTime << ENDL;
     int lat = (rand() % 500) + 150; //generate random latency between 150 - 650
-    std::ofstream data;
-    data.open("data.txt", ios::app);
+    FST data;
+    data.open("data.txt", APP);
 
     while(spikeTime >= freq) {
         data << lat << "\n";
@@ -50,38 +53,44 @@ int genSpike(int freq) {
         spikeTime -= freq;
         numAdded++;
     }
+    COUT << "added: " << numAdded << ENDL;
     data.close();
     return numAdded;
 }
 
 int generate(int time, int freq, int expected) {
-    srand(time(NULL));
+    //this still needs to integrate with time
+    srand(192);
     //num is going to be the number of data points generated
     int num = (time * 60 * 60) / freq;
 
     //prev is to make sure we do not generate spikes on top of one another... they need to have some space between them in order to
     //create the most accurate results.
     int prev = 0;
-    std::ofstream data;
-    data.open("data.txt", ios::app); //opens the file in append mode
+    FST data("data.txt");
+    data.close();
+    // data.open("data.txt", ios::app); //opens the file in append mode
 
     data << num << "\t" << time << "\t" << freq << '\t' << expected << "\n";
-    int lat = 0;
+    int lat = 40;
     for(int i = 0; i < num; i++) {
         //this is checking to see if the previous was greater than 30min ago and has a random 1/10 chance of triggering a spike in latency.
         //OR if there is an expected spike.
-        if((prev >= ((1 / (freq / 60)) * 30) && (rand() % 10) == 1 ) ||  time % expected == 0) {
+        if((prev >= ((1 / (freq / 60)) * 30) && (rand() % 10) == 1 )) { // ||  time % expected == 0
+            COUT << "making spike" << ENDL;
             int add = genSpike(freq);
             i += add - 1;
             prev = 0;
         }
         else {
-            lat = genLat(lat);
+            lat = genLat(40);
+            data.open("data.txt", APP); //this is not ideal... I need to better understand file writing...
             data << lat << '\n';
+            data.close();
             prev++;
         }
     }
-    data.close();
+
     return num;
 }
 
--- DataGen/src/generate.cpp
+++ DataGen/src/generate.cpp
@@ -1,4 +1,4 @@
-//Author: Chase Brown
+//Author: Chase Brown & Jacob Sardo
 //Email: cbrown54@nd.edu
 
 #include "../include/generator.h"
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
         COUT << "example: ./generate [time] [frequency]" << ENDL;
         COUT << "[time] = int value in hours ex. 24 or 48" << ENDL;
         COUT << "[frequency] = int value in seconds ex. 60 or 15" << ENDL;
-        COUT << "[expected] = int value in hours ex. 12 or 24 **[time] % [expected] == 0**" << ENDL
+        COUT << "[expected] = int value in hours ex. 12 or 24 **[time] % [expected] == 0**" << ENDL;
         COUT << "If we run './generate 12 60' this will generate 360 lines of data" << ENDL;
         COUT << "one line for each 60 seconds from 0 to 6 hours." << ENDL;
         return 0;
